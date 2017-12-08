a007e354073bed4f9b3d0944dac4479d1f3b2d5b
Ellie <elliewem@gmail.com>
2017-12-08 14:44:28 +0000

    Adds new methods to quit and advance the stage, extra part of chapter 1, the quit mechanism is hanging

--- adventure_script.rb
+++ adventure_script.rb
@@ -1,17 +1,23 @@
 class Run_Game
 
   def initialize
-    @counter = 0
-    @intro = true
+    @stage = 0
     @quit = true
     @answer = nil
     @rubies = 0
     @irb = false
   end
 
-  def end_game
-    while @quit == true
-      break
+  def advance_stage
+    @stage += 1
+  end
+
+  def user_input
+    @answer = gets.chomp.downcase
+    if @answer == "quit"
+      while @quit == true
+        break
+      end
     end
   end
 
@@ -69,36 +75,33 @@ class Run_Game
     puts "|                         Type ~start~ when you're ready to play                             |"
     puts "|____________________________________________________________________________________________|"
     puts
-    @answer = gets.chomp
-    if @answer.downcase == "start"
-      @counter += 1
-      main_game
-    end
-    if @answer.downcase == "quit"
-      end_game
+    user_input
+    if @answer == "start"
+      scene
     end
   end
 
-  def main_game
-    if @intro == true
+  def scene
       puts " ____________________________________________________________________________________________"
       puts "|                                                                                            |"
-      puts "|  You awaken with a jolt. You feel your chest rise and fall as you tentertively check if    |"
+      puts "|  You awaken with a jolt. You feel your chest rise and fall as you tentatively check if     |"
       puts "|  you're still alive... All good; you're still in the game! You've been out in the deep     |"
       puts "|  corners of space for far too long now, and you've had way too many crashes. You sometimes |"
       puts "|  wonder why YourBudgetSpacePilots(TM) ever let you out here in the first place... Hey!     |"
       puts "|  Who are you kidding? You're the best asronaut this side of Leamington Spa and you'll be   |"
       puts "|  darned-diddlied if anyone, even you, tells you otherwise!                                 |"
       puts "|                                                                                            |"
-      puts "|  You ~look at your IRB~ which has fallen off the shelf, and then turn your head to ~look   |"
+      puts "|  You ~look at the IRB~ which has fallen off the shelf, and then turn your head to ~look    |"
       puts "|  at the window~. You should probably get up off the floor and do something.                |"
       puts "|____________________________________________________________________________________________|"
       puts
-      @intro == false
-      @answer = gets.chomp
-    end
-    while @counter == 1
-      if @answer.downcase == "look at your irb"
+      chapter1
+  end
+
+  def chapter1
+    while @stage == 0
+      user_input
+      if @answer == "look at the irb"
         puts " ____________________________________________________________________________________________"
         puts "|                                                                                            |"
         puts "|  You get up and walk over to the IRB gun. It was a gift from your dad, the best astronaut  |"
@@ -107,18 +110,38 @@ class Run_Game
         puts "|                                                                                            |"
         puts "|  You wipe the dust off of the IRB and wonder what it could possibly do. You've never       |"
         puts "|  quite figured that one out, even though old man Jiggins said you'd find out soon. That    |"
-        puts "|  was twenty years ago... You go to put it back on the top shelf, when all of a sudden      |"
-        puts "|  the monitor start flashing \"2.4.1 :001\". Maybe you should ~take the IRB~ with you and     |"
-        puts "|  ~look at the window~.                                                                     |"
+        puts "|  was ten years ago... You go to put it back on the top shelf, when all of a sudden the     |"
+        puts "|  monitor starts flashing \"2.4.1 :001\". Maybe you should ~press some buttons~               |"
         puts "|____________________________________________________________________________________________|"
-        @answer = gets.chomp
+        user_input
+        if @answer == "press some buttons"
+          puts " ____________________________________________________________________________________________"
+          puts "|                                                                                            |"
+          puts "|  You look at the IRB and notice it's not so scary after all. You have a brief moment of    |"
+          puts "|  self reflection; maybe you hid it away on the top shelf because you didn't want to face   |"
+          puts "|  that you still didn't understand it after this many years. Kind of like that time at      |"
+          puts "|  Astronaut University when you didn't understand how the toilets worked so you used to     |"
+          puts "|  drive all the way home to use your own, and it got worse and worse because you thought    |"
+          puts "|  everyone would think you were weird if you had to ask how the toilet worked half way      |"
+          puts "|  through the semester because they\'d all realise you hadn't been to the toilet on campus   |"
+          puts "|  for three months... six months. . . a year .  .  .                                        |"
+          puts "|                                                                                            |"
+          puts "|  The monitor keeps flashing \"2.4.1 :001\" and you feel it mocking you. \"Ha ha! It was       |"
+          puts "|  totally nothing like that! This IRB is just a piece a trash that is un-understandable!\"   |"
+          puts "|  you proudly shout to no one. You're so committed to this idea that you throw it across    |"
+          puts "|  the room and immediately regret it.                                                       |"
+          puts "|                                                                                            |"
+          puts "|  "
+          puts "|____________________________________________________________________________________________|"
+          user_input
+        end
       end
-      if @answer.downcase == "look at the window"
-        puts "You look at the window"
-        @answer = gets.chomp
-      end
-      if @answer.downcase == "quit"
-        end_game
+      if @answer == "look at the window"
+        puts " ____________________________________________________________________________________________"
+        puts "|                                                                                            |"
+        puts "|  You look over at the window. Not much to see here but infinite darkness. Maybe you        |"
+        puts "|  should take a ~look at the IRB~                                                          |"
+        puts "|____________________________________________________________________________________________|"
       end
     end
   end
