45e29f57955d15ab9c4cf0ec3b18eabde203f3a7
Ellie <elliewem@gmail.com>
2017-12-14 16:18:56 +0000

    Amend README

---
+++ lib/script.rb
@@ -0,0 +1,10 @@
+#def check_vowel(string, position)
+#  puts "yay" if ["a", "e", "i", "o", "u"].include?string.split("")[position]
+#end
+
+#print "Gcd noUUGdxvvupzOeUgIuehLoHuAeEieIEJ izB EAu lIo".split("")[37]
+
+str = "1 2 4 2 7"
+str.split(" ").each do |x|
+  x.to_i + 1
+end
